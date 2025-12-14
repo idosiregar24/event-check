c3ed0dc0817d09ef6c654efbc55fc62d575634d5
IdoRefaelSiregar24 <ido24si@mahasiswa.pcr.ac.id>
2025-12-14 11:03:26 +0700

    Penambahan seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Ido Refael Siregar',
+            'email' => 'ido24si@mahasiswa.pcr.ac.id',
+            'password' => Hash::make('Asdfg123@#'),
+            'role' => 'super_admin',
+        ]);
+    }
+}
